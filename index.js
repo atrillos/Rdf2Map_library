03439b61b25b9fb29b6b88697f4006b468df4d24
Luis Fernandes <sednanref2004@gmail.com>
2017-06-18 00:43:21 +0200

    Removed ola k ase

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const Inert = require('inert');
 var config = {};
 var server = new hapi.Server(config);
 const portToUse = process.env.PORT || 5000;
-console.log(portToUse);
 server.connection({ port: ~~process.env.PORT || 5000, host: '0.0.0.0' });
 server.register([Inert], (err) => {
 
@@ -31,7 +30,6 @@ server.register([Inert], (err) => {
         method: 'GET',
         path: '/',
         handler: function (request, reply) {
-            console.log('ola k ase');
             reply.file('RDF2Map.html');
         }
     });
@@ -41,25 +39,3 @@ server.register([Inert], (err) => {
         console.log('Server running at:', server.info.uri);
     });
 });
-/*
-let express = require('express');
-let app = express();
-let path = require('path');
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  //response.render('RDF2Map.html');
-  response.sendFile(path.join(__dirname + '/RDF2Map.html'));
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-*/
