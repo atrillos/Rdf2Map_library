57a462482066b097121ff32d940f83bf9aa92a07
Luis Fernandes <sednanref2004@gmail.com>
2017-06-17 23:14:54 +0200

    Restructuring everything. Hapi js server working!

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(5000, function(){
-    console.log('Server running on 80...');
+const hapi = require('hapi');
+const Inert = require('inert');
+//const Lout = require('lout');
+//const Vision = require('vision');
+//const Routes = require('./routes');
+
+var config = {};
+var server = new hapi.Server(config);
+server.connection({ port: 5000, host: 'localhost' });
+server.register([Inert], (err) => {
+
+    if (err) {
+        console.error('Failed loading plugins');
+        process.exit(1);
+    }
+	
+    server.route({
+        path: "/resources/{path*}",
+        method: "GET",
+        handler: {
+        directory: {
+            path: "./resources",
+            listing: false,
+            index: false
+        }
+    }});
+    
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            console.log('ola k ase');
+            reply.file('RDF2Map.html');
+        }
+    });
+
+    server.start(() => {
+
+        console.log('Server running at:', server.info.uri);
+    });
 });
+/*
+let express = require('express');
+let app = express();
+let path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  //response.render('RDF2Map.html');
+  response.sendFile(path.join(__dirname + '/RDF2Map.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+*/
