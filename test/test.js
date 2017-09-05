9b5675bccc7b714d68012d7f987e1595d97ec1a6
Luis Daniel Fernandes Rotger <Luis.Daniel.Fernandes.Rotger@iais.fraunhofer.de>
2017-09-05 10:30:19 +0200

    Added tests.

--- test/test.js
+++ test/test.js
@@ -1,91 +1,200 @@
+// Imports of assert, rdfstore, fs and rdf2map.
 let assert = require('assert');
 let rdfstore = require('rdfstore');
 let fs = require('fs');
-let jsdom = require('jsdom');
-let rdf2map = require('../resources/rdf2map.js');
-const { JSDOM } = jsdom;
-const { Script } = require('vm');
+let rdf2map = require('../resources/rdf2map.js').RDF2Map;
 
+// Create globals so leaflet can load
+global.window = {
+	screen: {
+		deviceXDPI: 1,
+		logicalXDPI: 1
+	}
+};
+global.document = {
+  	documentElement: {
+		style: {}
+	},
+	getElementsByTagName: function() { return []; },
+	createElement: function() { return {}; }
+};
+global.navigator = {
+  userAgent: 'nodejs',
+  platform: []
+};
+// Import leaflet
+let L = require('leaflet');
 
-console.log(rdf2map);
 describe('RDF2Map', () => {
-	describe('#indexOf()', (suite) => {		
-		let s = null;
-		let dom = null;
-		let window = null;
-		let rdfstore = null;
-		let rdf2map = null;
-		let RDF2Map = null;
-		let leaflet = null;
-		let lmc = null;
 
-		before((done) => {
-			console.log(rdf2map);
-			fs.readFile('./resources/rdf2map.js', 'utf8', (err, data) => {
-				if (err) {
-					return console.error(err);
-				}
-				let RDF2Map = rdf2map.RDF2Map;
-				s = new Script(data);
-				JSDOM.fromFile('test/testhtml.html', { runScripts: 'outside-only' }).then( res => {
-					dom = res;
-					window = dom.window;
-					done();
-				});
-				
-			});
-		});
-
-		// RDFStore Script
-		before((done) => {
-			fs.readFile('./resources/rdfstore.js', 'utf8', (err, data) => {
-				rdfstore = data;
-				rdfstoreScript = new Script(data);
-				done();
-			});
-		});
-
-		// Leaflet Script
-		before((done) => {
-			fs.readFile('./resources/leaflet.js', 'utf8', (err, data) => {
-				leaflet = data;
-				done();
-			});
-		});
-
-		// Rdf2map Script
-		before((done) => {
-			fs.readFile('./resources/rdf2map.js', 'utf8', (err, data) => {
-				rdf2map = data;
-				done();
-			});
-		});
-
-		// leaflet marker clusters Script
-		before((done) => {
-			fs.readFile('./resources/leaflet.markercluster.js', 'utf8', (err, data) => {
-				lmc = data;
-				lmc = new Script(lmc);
-				done();
-			});
-		});
-
-		it('Should retrieve information from external accessible resources', () => {
-			
-			let info = rdf2map.getInfoSubjects(["http://dbpedia.org/resource/Germany", "http://dbpedia.org/resource/Bonn"]);
-			console.log(info);
-			//dom.runVMScript(rdfstore);	
-			//console.log(lmc);
-			//console.log(dom.serialize());
-
-/*			
-			console.log('//////');
-			console.log(dom.serialize());
-			console.log('////');
-			console.log(s);
-			*/
-			assert.equal(-1, [1,2,3].indexOf(9));
-			
-		});
+	it('Should create markers for location points.', () => {
+		// Read test file
+		fs.readFile('./Turtle\ Files/VzlaCities.ttl', 'utf8', (err, data) => {
+			// Create the store
+		   	rdfstore.create((err, store) => {
+		   		// Load data in store
+		   		store.load('text/turtle', data, (err, res) => {
+		   			if (err) {
+		   				console.log('Error loading turtle file', err);
+		   			}
+					rdf2map.processMarkers(store).then((res) => {
+						assert.equal(10, res.length);
+					});
+		   		});
+		   	});
+        });		
 	});
+
+	it('Should create Icon markers for location points.', () => {
+		// Read test file
+		fs.readFile('./Turtle\ Files/icons.ttl', 'utf8', (err, data) => {
+			// Create the store
+		   	rdfstore.create((err, store) => {
+		   		// Load data in store
+		   		store.load('text/turtle', data, (err, res) => {
+		   			if (err) {
+		   				console.log('Error loading turtle file', err);
+		   			}
+					rdf2map.processIcons(store).then((res) => {
+						assert.equal(8, res.length);
+					});
+		   		});
+		   	});
+        });
+	});
+
+	it('Should create Polygons for location areas.', () => {
+		// Read test file
+		fs.readFile('./Turtle\ Files/polygons.ttl', 'utf8', (err, data) => {
+			// Create the store
+		   	rdfstore.create((err, store) => {
+		   		// Load data in store
+		   		store.load('text/turtle', data, (err, res) => {
+		   			if (err) {
+		   				console.log('Error loading turtle file', err);
+		   			}
+					rdf2map.processPolygons(store).then((res) => {
+						assert.equal(4, res.length);
+					});
+		   		});
+		   	});
+        });
+	});
+
+	it('Should create Paths for location areas.', () => {
+		// Read test file
+		fs.readFile('./Turtle\ Files/paths.ttl', 'utf8', (err, data) => {
+			// Create the store
+		   	rdfstore.create((err, store) => {
+		   		// Load data in store
+		   		store.load('text/turtle', data, (err, res) => {
+		   			if (err) {
+		   				console.log('Error loading turtle file', err);
+		   			}
+					rdf2map.processPaths(store).then((res) => {
+						assert.equal(3, res.length);
+					});
+		   		});
+		   	});
+        });
+	});
+
+	it('Should create the exact amount of different geo Concepts.', () => {
+		// Read test file
+		fs.readFile('./Turtle\ Files/map.ttl', 'utf8', (err, data) => {
+			// Create the store
+		   	rdfstore.create((err, store) => {
+		   		// Load data in store
+		   		store.load('text/turtle', data, (err, res) => {
+		   			if (err) {
+		   				console.log('Error loading turtle file', err);
+		   			}
+					rdf2map.processMarkers(store).then((res) => {
+						assert.equal(2, res.length);
+					});
+					rdf2map.processIcons(store).then((res) => {
+						assert.equal(5, res.length);
+					});
+					rdf2map.processPolygons(store).then((res) => {
+						assert.equal(1, res.length);
+					});
+					rdf2map.processPaths(store).then((res) => {
+						assert.equal(1, res.length);
+					});
+		   		});
+		   	});
+        });
+    });
+
+    it('Should create no markers if there is no marker in the file.', () => {
+		// Read test file
+		fs.readFile('./Turtle\ Files/icons.ttl', 'utf8', (err, data) => {
+			// Create the store
+		   	rdfstore.create((err, store) => {
+		   		// Load data in store
+		   		store.load('text/turtle', data, (err, res) => {
+		   			if (err) {
+		   				console.log('Error loading turtle file', err);
+		   			}
+					rdf2map.processPaths(store).then((res) => {
+						assert.equal(0, res.length);
+					});
+				});
+	   		});
+	   	});
+    });
+
+    it('Should create no icons if there is no Icons in the file.', () => {
+		// Read test file
+		fs.readFile('./Turtle\ Files/VzlaCities.ttl', 'utf8', (err, data) => {
+			// Create the store
+		   	rdfstore.create((err, store) => {
+		   		// Load data in store
+		   		store.load('text/turtle', data, (err, res) => {
+		   			if (err) {
+		   				console.log('Error loading turtle file', err);
+		   			}
+					rdf2map.processIcons(store).then((res) => {
+						assert.equal(0, res.length);
+					});
+	   			});
+	   		});
+    	});
+    });
+
+    it('Should create no polygons if there is no polygons in the file.', () => {
+		// Read test file
+		fs.readFile('./Turtle\ Files/VzlaCities.ttl', 'utf8', (err, data) => {
+			// Create the store
+		   	rdfstore.create((err, store) => {
+		   		// Load data in store
+		   		store.load('text/turtle', data, (err, res) => {
+		   			if (err) {
+		   				console.log('Error loading turtle file', err);
+		   			}
+					rdf2map.processPolygons(store).then((res) => {
+						assert.equal(0, res.length);
+					});
+				});
+	   		});
+	   	});
+    });
+
+    it('Should create no paths if there is no paths in the file.', () => {
+		// Read test file
+		fs.readFile('./Turtle\ Files/VzlaCities.ttl', 'utf8', (err, data) => {
+			// Create the store
+		   	rdfstore.create((err, store) => {
+		   		// Load data in store
+		   		store.load('text/turtle', data, (err, res) => {
+		   			if (err) {
+		   				console.log('Error loading turtle file', err);
+		   			}
+					rdf2map.processPaths(store).then((res) => {
+						assert.equal(0, res.length);
+					});
+				});
+	   		});
+	   	});
+    });
 });
